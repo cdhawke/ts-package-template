af2a8209964e96148b66300f4023cfb2d09c1156
Christopher Hawke <69921547+cdhawke@users.noreply.github.com>
2023-01-03 13:03:03 -0500

    feat: adding basic bin and renaming package

--- scripts/bin.js
+++ scripts/bin.js
@@ -1,10 +1,34 @@
 #!/usr/bin/env node
 
-const yargs = require('yargs');
-var exec = require('child_process').exec;
-if (yargs.argv.react) {
-  console.log('Installing React dependencies');
-  exec(
-    'yarn add -D eslint-plugin-react-hooks eslint-plugin-react @types/react @testing-library/react-hooks @testing-library/react'
-  );
+const { execSync } = require('child_process');
+const runCommand = (command) => {
+  try {
+    execSync(`${command}`, { stdio: 'inherit' });
+  } catch (e) {
+    console.error(`Failed to execute ${command}`, e);
+    return false;
+  }
+  return true;
+};
+
+const repo = process.argv[2];
+const gitCheckout = `git clone --depth 1 https://github.com/cdhawke/ts-package-template.git ${repo}`;
+const installDeps = `cd ${repo} && yarn install`;
+console.log('Cloning...');
+const checkedOut = runCommand(gitCheckout);
+if (!checkedOut) {
+  process.exit(-1);
 }
+
+console.log('Installing Dependencies...');
+const depsInstalled = runCommand(installDeps);
+if (!depsInstalled) {
+  process.exit(-1);
+}
+
+// if (yargs.argv.react) {
+//   console.log('Installing React dependencies');
+//   exec(
+//     'yarn add -D eslint-plugin-react-hooks eslint-plugin-react @types/react @testing-library/react-hooks @testing-library/react'
+//   );
+// }
